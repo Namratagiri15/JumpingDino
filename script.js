6542169b9c3ba376177ecc93d1f9dfe3c5813baf
Namratagiri15 <37000432+Namratagiri15@users.noreply.github.com>
2020-08-23 16:37:54 +0530

    Update script.js
    Removed Reload page and added better way to handle start game function.

--- script.js
+++ script.js
@@ -2,27 +2,35 @@ const character = document.querySelector('#character');
 const block = document.querySelector('#block');
 let score = document.querySelector('#score');
 let counter = 0;
+let startFlag = false;
+block.style.animation = "none";
 
 function jump() {
-   character.classList.add("animate");
-   setTimeout(() => {
-       character.classList.remove("animate");
-       counter++;
-        score.innerHTML= "Your Score is : "+(counter-1);
+   if(startFlag) {
+    character.classList.add("animate");
+    setTimeout(() => {
+        character.classList.remove("animate");
+        counter++;
+         score.innerHTML= "Your Score is : "+(counter-1);
+    }
+        ,500)
    }
-       ,500)
 }
 
 function start() {
-    window.location.reload();
+    startFlag = true;
+    block.style.animation = "block 1s infinite";
 }
 
 const checkDead = setInterval(function(){
-    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-    if(blockLeft< 20 && blockLeft> 0 && characterTop >= 130) {
-        block.style.animation = "none";
-        counter = 0;
-        alert("Sorry! You lost the Game :(");
+    if(startFlag) {
+        const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+        const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+        if(blockLeft< 20 && blockLeft> 0 && characterTop >= 130) {
+            block.style.animation = "none";
+            counter = 0;
+            alert("Sorry! You lost the Game :(");
+            startFlag = false;
+        }
     }
 },10);
